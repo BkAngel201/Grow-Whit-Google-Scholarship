c1e8731dbb8b605e09c8e5260a010aeae4098ada
BkAngel201 <angelcouso91@gmail.com>
2018-02-24 21:35:28 -0500

    made some big
    -select tools on keypress
    -popup text on the tools and file buttons
    -change cursors for the tools

--- project-pixel-art-maker-starter-master/project-pixel-art-maker-starter-master/designs.js
+++ project-pixel-art-maker-starter-master/project-pixel-art-maker-starter-master/designs.js
@@ -11,77 +11,6 @@ let gridColorPickerValue = $('#gridColorPicker').val();
 let cellHeightAndWidth = 10;
 let toolSelected;
 
-//Style behavior
-//open and close the tool bar
-$(".toggle-pos-button").on('click',function(){
-  $(this).parent().toggleClass('closed');
-  $(this).children('i').toggleClass('fa-angle-double-right');
-});
-
-//tools button selection and set the selected tool
-$(".tools-button").on('click',function(){
-  $(".tools-button").removeClass('selected');
-  $(this).toggleClass('selected');
-  toolSelected = $(this).attr('tool');
-});
-
-//open the popup windows for the specific file button
-$(".file-button").on('click',function(){
-  if($(this).attr('action') === "create") {
-    $('#'+$(this).attr('action')).toggleClass('visible');
-  } else if($(this).attr('action') === "reset") {
-    $('#'+$(this).attr('action')).toggleClass('visible');
-  }
-});
-
-//close the popup window with the x button
-$('.popup-window-close').on('click',function(){
-  $(this).parents('.popup-window').toggleClass('visible');
-});
-
-//cancel and accept the reset grid function
-$('button.cancel').on('click',function(){
-  $(this).parents('.popup-window').toggleClass('visible');
-});
-$('button.accept').on('click',function(){
-  $(this).parents('.popup-window').toggleClass('visible');
-  $('td').css('background-color', backColorPickerValue);
-});
-
-
-$('input[type=color]').change(function(){
-  if($(this).attr('id') === "backColorPicker") {
-    //setting the background color of the table
-    //setting the backColorPickerValue when the #colorPicker change
-    $("table").css('background-color', $(this).val());
-    backColorPickerValue = $(this).val();
-  } else if($(this).attr('id') === "gridColorPicker") {
-    //setting the border color of the table
-    //setting the gridColorPickerValue when the #colorPicker change
-    $("td").css('border-color', $(this).val());
-    gridColorPickerValue = $(this).val();
-  } else if($(this).attr('id') === "colorPicker") {
-    //setting the colorPickerValue when the #colorPicker change
-    colorPickerValue = colorPickerElement.val();
-  }
-});
-
-$('.about-button').on('click',function(){
-  $('.popup-window#about').toggleClass('visible');
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 //TODO: fix the height and width of the cell if the grid is biger than the scrwwn size to prevent overflowing
@@ -114,9 +43,12 @@ function makeGrid() {
 calcCellDimention();
 $("table").css('background-color', backColorPickerValue);
 $("td").css('border-color', gridColorPickerValue);
+pixelCanvasElement.find('td').attr('backgroundColor', backColorPickerValue);
 $('#pixel_canvas td').css("height", cellHeightAndWidth + "px");
 $('#pixel_canvas td').css("width", cellHeightAndWidth + "px");
 }
+
+
 
 //******************************//
 //                              //
@@ -136,19 +68,129 @@ $('#sizePicker').submit(function(event) {
   //if the width or height were corrected, show the information to the user
 });
 
-pixelCanvasElement.on("click mousedown mouseover", "td", function(event){
+pixelCanvasElement.on("mousedown mouseover", "td", function(event){
   event.preventDefault();
   if (event.buttons == 1) {
     if (toolSelected === "brush") {
+      $(event.target).attr('backgroundColor', colorPickerValue);
       $(event.target).css('background-color', colorPickerValue);
+
     } else if (toolSelected === "eraser") {
       $(event.target).css('background-color', backColorPickerValue);
+    }
+    if (toolSelected === "dropper") {
+      colorPickerElement.val($(event.target).attr('backgroundColor'));
+      colorPickerValue = $(event.target).attr('backgroundColor');
+      $(".fa-paint-brush").parent().click();
     }
   }
 });
 
 
-// //changing the td background for colorPickerValue
-// pixelCanvasElement.on('click','td', function() {
-//   $(this).css('background-color', colorPickerValue);
-// });
+//Style behavior
+//open and close the tool bar
+$(".toggle-pos-button").on('click',function(){
+  $(this).parent().toggleClass('closed');
+  $(this).children('i').toggleClass('fa-angle-double-right');
+});
+
+//tools button selection and set the selected tool
+$(".tools-button").on('click',function(){
+  let url = "";
+  $(".tools-button").removeClass('selected');
+  $(this).toggleClass('selected');
+  toolSelected = $(this).attr('tool')
+  if(toolSelected === "brush") {
+    url = "https://s26.postimg.org/cv9eddj4p/brush.png";
+  } else if(toolSelected === "eraser") {
+    url = "https://s26.postimg.org/thqyn1c8p/eraser.png";
+  } else if(toolSelected === "dropper") {
+    url = "https://s26.postimg.org/w0cnn5d89/dropper.png";
+  }
+  $("body").css("cursor","url(" + url + "), default");
+});
+
+//behavior of popup text on tools and file buttons
+//when the mouse is over a button show the popup text
+$("a[alt=popup]").on('mouseover', function(event){
+  //change the text in the text on the popup, since file and tools button use different attributes to define what they do
+  if($(this).hasClass("tools-button")) {
+    $(".popup-info-text").html($(this).attr("tool") + " tool (" + $(this).attr("Key") + ")");
+  } else if($(this).hasClass("file-button")) {
+    $(".popup-info-text").html($(this).attr("action") + " grid");
+  }
+  //place the popup text in the right place calculating the position using the trigger position and the dimentions of the elements
+  $('.popup-info-text').css({
+    "top" : parseInt($(this).offset().top + ($(this).height() - $(".popup-info-text").height())) + "px",
+    "left" : parseInt($(this).offset().left + $(this).width() + 5) + "px"
+  });
+  //set the popup text visible
+  $(".popup-info-text").addClass('visible');
+});
+
+//when the mouse leave a button hidde the popup text
+$("a[alt=popup]").on('mouseout', function(event){
+  //set the popup text not visible
+  $(".popup-info-text").removeClass('visible');
+});
+
+//open the popup windows for the specific file button
+$(".file-button").on('click',function(){
+  if($(this).attr('action') === "create") {
+    $('#'+$(this).attr('action')).toggleClass('visible');
+  } else if($(this).attr('action') === "reset") {
+    $('#'+$(this).attr('action')).toggleClass('visible');
+  }
+});
+
+//close the popup window with the x button
+$('.popup-window-close').on('click',function(){
+  $(this).parents('.popup-window').toggleClass('visible');
+});
+
+//cancel and accept the reset grid function
+$('button.cancel').on('click',function(){
+  $(this).parents('.popup-window').toggleClass('visible');
+});
+$('button.accept').on('click',function(){
+  $(this).parents('.popup-window').toggleClass('visible');
+  $('td').css('background-color', backColorPickerValue);
+});
+
+
+$('input[type=color]').change(function(){
+  if($(this).attr('id') === "backColorPicker") {
+    //setting the background color of the table
+    //setting the backColorPickerValue when the input type=color change
+    $("table").css('background-color', $(this).val());
+    backColorPickerValue = $(this).val();
+  } else if($(this).attr('id') === "gridColorPicker") {
+    //setting the border color of the table
+    //setting the gridColorPickerValue when the input type=color change
+    $("td").css('border-color', $(this).val());
+    gridColorPickerValue = $(this).val();
+  } else if($(this).attr('id') === "colorPicker") {
+    //setting the colorPickerValue when the #colorPicker change
+    colorPickerValue = colorPickerElement.val();
+  }
+});
+
+
+$('.about-button').on('click',function(){
+  $('.popup-window#about').toggleClass('visible');
+});
+
+//event listener for keydown to select tools with specific keys
+$(document).on("keydown", function(event){
+  //prevent select any tool if the popup windows is visible
+  if(!$('.popup-window').hasClass("visible")){
+    if (event.which == 66) {
+      $(".tools-button[tool=brush]").click();
+    } else if (event.which == 69) {
+      $(".tools-button[tool=eraser]").click();
+    } else if (event.which == 68) {
+      $(".tools-button[tool=dropper]").click();
+    }
+  }
+
+});
